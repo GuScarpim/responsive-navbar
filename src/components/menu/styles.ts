de782b4dd0a1ad85080a4e574fd6db16086421fa
guscarpim <gustavoscarpim@gmail.com>
2021-09-30 21:52:24 -0300

    refactor: Refactor in styles

--- src/components/menu/styles.ts
+++ src/components/menu/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 interface INav {
   open: boolean;
-  href?: string;
 }
 
 export const StyledBurger = styled.div<INav>`
@@ -13,21 +12,25 @@ export const StyledBurger = styled.div<INav>`
   right: 20px;
   z-index: 20;
   display: none;
-  font-family: 'Zilla Slab';
+  cursor: pointer;
+  font-family: 'Zilla Slab', sans-serif;
 
   @media (max-width: 768px) {
     display: flex;
     justify-content: space-around;
     flex-flow: column nowrap;
   }
-  div {
+`
+
+export const Menus = styled.div<INav>`
     width: 2rem;
     height: 0.25rem;
-    background-color: ${(props) => props.open ? '#000' : '#000'};
+    background-color: #000;
     border-radius: 10px;
     transform-origin: 1px;
     transition: all 0.3s linear;
     cursor: pointer;
+
     &:nth-child(1) {
       transform: ${(props) => props.open ? 'rotate(45deg)' : 'rotate(0)'};
     }
@@ -38,11 +41,10 @@ export const StyledBurger = styled.div<INav>`
     &:nth-child(3) {
       transform: ${(props) => props.open ? 'rotate(-45deg)' : 'rotate(0)'};
     }
-  }
 `
 
 export const Nav = styled.nav`
-  font-family: 'Zilla Slab';
+  font-family: 'Zilla Slab', sans-serif;
   height: 100%;
   display: flex;
   justify-content: space-between;
@@ -53,22 +55,10 @@ export const Nav = styled.nav`
   @media (max-width: 678px) {
     width: 100vw;
   }
-
-  span {
-    font-size: 30px;
-    @media only screen and (max-width: 600px) {
-      font-size: 20px;
-      :nth-child(2) {
-        font-size: 16px !important;
-        margin-top: 0px !important;
-      }
-    }
-  }
-
 `
 
 export const Ul = styled.ul<INav>`
-  font-family: 'Zilla Slab';
+  font-family: 'Zilla Slab', sans-serif;
   list-style: none;
   display: flex;
   flex-flow: row nowrap;
@@ -93,10 +83,6 @@ export const Ul = styled.ul<INav>`
     }
   }
 
-  li {
-    padding: 18px 10px;
-  }
-
   @media (max-width: 768px) {
     flex-flow: column nowrap;
     background-color: #fdfdfdfa;
@@ -110,16 +96,21 @@ export const Ul = styled.ul<INav>`
     transition: transform 0.3s ease-in-out;
     z-index: 9;
     justify-content: normal;
-
-    li {
-      color: #000;
-      margin-right: 34px;
-
-      &:hover {
-        color: #0DADEA;
-      }
-    }
   }
+`
+
+export const Li = styled.li`
+  padding: 18px 10px;
+  outline: none;
+
+  @media (max-width: 768px) {
+    color: #000;
+    margin-right: 34px;
+
+    &:hover {
+      color: #0DADEA;
+    }
+}
 `
 
 export const Logo = styled.img`
@@ -145,30 +136,19 @@ export const LogoUl = styled.img`
     object-fit: contain;
   }
 `
+
 export const Icon = styled.div`
   width: 100vw;
   height: calc(100vh - 112px);
   display: flex;
   justify-content: center;
   align-items: center;
+`
 
-  img {
-    width: 150px;
-    height: 150px;
-    pointer-events: none;
-    object-fit: contain;
-
-    @media (prefers-reduced-motion: no-preference) {
-      animation: App-logo-spin infinite 20s linear;
-    }
-  }
-
-    @keyframes App-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-  }
+export const Image = styled.img`
+  width: 150px;
+  height: 150px;
+  pointer-events: none;
+  object-fit: contain;
+  animation: Spin infinite 20s linear;
 `
